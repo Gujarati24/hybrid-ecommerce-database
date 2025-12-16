3352a427dbadb618ca1cb73e3962c8819829fb6f
Gujarati24 <tomikgujarati01@gmail.com>
2025-12-16 14:50:35 +0100

    Update sample_data.sql

--- sql/sample_data.sql
+++ sql/sample_data.sql
@@ -1,1 +1,9 @@
+-- Sample data for hybrid e-commerce Project
 
+INSERT INTO users ( name, email) VALUES
+('Alice', 'alice@example.com'),
+('Bob', 'bob@example.com');
+
+INSERT INTO products (name, price, stock) VALUES
+('Laptop', 75000, 10),
+('Mouse', 500, 50);
